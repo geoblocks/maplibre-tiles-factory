9ac22e82882d367f8cc94abb1a17cb720be9e1ec
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2026-05-08 16:30:30 +0200

    Split source

--- src/demos/MapLibreReprojectedSource.ts
+++ src/demos/MapLibreReprojectedSource.ts
@@ -1,4 +1,4 @@
-import maplibregl from "maplibre-gl";
+import type maplibregl from "maplibre-gl";
 import ImageTileSource from "ol/source/ImageTile.js";
 import { createXYZ } from "ol/tilegrid.js";
 import type { Projection } from "ol/proj.js";
